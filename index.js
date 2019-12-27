c3cd868b1a83974e9207de4037d1896741e7b65f
Karen V <karenvo@usc.edu>
2019-12-27 06:06:43 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solutions in this file
+const gifts = ["teddy bear", "drone", "doll"];
+
+function wrapGifts(gifts) {
+  for (let i = 0; i < gifts.length; i++) {
+    console.log(`Wrapped ${gifts[i]} and added a bow!`);
+  }
+  return gifts;
+}
+wrapGifts(gifts);
+
+function writeCards(names, type) {
+  for (let i = 0; i < names.length; i++) {
+    console.log(`Thank you, ${name[i]}, for the wonderful ${type} gift!`);
+  }
+}
+writeCards([ 'Lisa', 'Kaitlin', 'Jan' ], "birthday");
+
+for (let countup = 0; countup < 10; countup++) {
+  console.log(countup);
+}
